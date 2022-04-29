f51071994bcde4047ce3d15f16847e144d805083
James Elliott <james-d-elliott@users.noreply.github.com>
2022-04-29 14:53:20 +1000

    test(suites): make redis restart last

--- internal/suites/suite_traefik2_test.go
+++ internal/suites/suite_traefik2_test.go
@@ -29,7 +29,7 @@ func (s *Traefik2Suite) TestCustomHeaders() {
 	suite.Run(s.T(), NewCustomHeadersScenario())
 }
 
-func (s *Traefik2Suite) TestShouldKeepSessionAfterRedisRestart() {
+func (s *Traefik2Suite) TestXLastShouldKeepSessionAfterRedisRestart() {
 	ctx, cancel := context.WithTimeout(context.Background(), 45*time.Second)
 	defer func() {
 		cancel()
